b67d0d6995ea3e20e63f46bef532ebb2fa176aa1
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-11-19 11:48:09 -0800

    Add ToDoItem Title logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+    const ToDoApp = () => {
+        
+        function MakeToDoItem (title) {
+            const titleWrapper = {
+                title,
+                renameTitle: function (newTitle) {
+                    this.title = newTitle
+                }
+            }
+            return {titleWrapper}
+        }
+    }
