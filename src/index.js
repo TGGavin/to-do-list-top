f6fe7133dd53dad45113e839e42182d8274719b3
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-11-21 09:05:40 -0800

    Add to do item logic for dates and time until due

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 import "./style.css";
+const { formatDistance } = require("date-fns");
 
     const ToDoApp = () => {
         
         function toDoItem () {
 
-            const MakeToDoItem = (title, desc, notes, priority, progress) => {
+            const MakeToDoItem = (title, desc, notes, priority, progress, dueDate) => {
                 if (typeof progress !== "boolean") {
                     throw new Error("Progress parameter must be set in boolean")
                 }
-
                 const toDoObj = {};
+
+                const toDoDates = GetToDoDates(dueDate)
+
                 const renameToDoTitle =  renameTitle.bind(toDoObj);
                 const rewriteToDoDesc = rewriteDesc.bind(toDoObj);
                 const rewriteToDoNotes = rewriteNotes.bind(toDoObj);
                 const setToDoPriority = setPriority.bind(toDoObj);
                 const updateToDoProgress = updateProgress.bind(toDoObj);
+                const updateToDoDates = updateDates.bind(toDoObj);
 
                 return Object.assign(toDoObj, { 
                     title, 
                     desc, 
                     notes,
                     priority,
-                    progress
+                    progress,
+                    toDoDates,
                 }, 
                 { 
                     renameToDoTitle, 
@@ -29,6 +34,7 @@ import "./style.css";
                     rewriteToDoNotes,
                     setToDoPriority,
                     updateToDoProgress,
+                    updateToDoDates,
                 })
             };
 
@@ -51,6 +57,23 @@ import "./style.css";
             function updateProgress () {
                 this.progress = !this.progress
             }
+
+            function updateDates (newDueDate) {
+                this.toDoDates = GetToDoDates(newDueDate);
+            }
+
+            function GetToDoDates (dueDate) {
+                const itemTOC = new Date();
+                const itemDueDate = new Date(dueDate);
+                const timeUntilDue = formatDistance(itemDueDate, itemTOC);
+                return {
+                    itemTOC,
+                    itemDueDate,
+                    timeUntilDue,
+                }
+                
+                
+            };
 
             return {
                 MakeToDoItem,
